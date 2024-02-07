896d98dce4b3f79a04dc80eff5f2b7b816380f41
ACendlewski <adamc01@interia.eu>
2024-02-07 16:12:09 +0100

    +Fight-buttons, -URP

---
+++ Assets/Scripts/Fight/FighterAction.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FighterAction : MonoBehaviour
+{
+    private GameObject hero;
+    private GameObject enemy;
+
+    [SerializeField]
+    private GameObject meleePrefab;
+
+    [SerializeField]
+    private GameObject rangePrefab;
+
+    [SerializeField]
+    private Sprite faceIcon;
+
+    private GameObject currentAttack;
+
+    void Awake()
+    {
+        hero = GameObject.FindGameObjectWithTag("Hero");
+        enemy = GameObject.FindGameObjectWithTag("Enemy");
+    }
+    public void SelectAttack(string btn)
+    {
+        GameObject victim = hero;
+        if (tag == "Hero")
+        {
+            victim = enemy;
+        }
+        if (btn.CompareTo("melee") == 0)
+        {
+            //meleePrefab.GetComponent<AttackScript>().Attack(victim);
+            Debug.Log("Melee");
+        }
+        else if (btn.CompareTo("range") == 0)
+        {
+            //rangePrefab.GetComponent<AttackScript>().Attack(victim);
+            Debug.Log("Range");
+        }
+        else
+        {
+            Debug.Log("Run");
+        }
+    }
+}
