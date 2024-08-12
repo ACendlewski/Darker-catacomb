253b2b534b0bef5c4476020b32c2e3f14ebe6a1c
ACendlewski <adamc01@interia.eu>
2024-08-12 19:11:10 +0200

    characters.json

---
+++ Assets/Scripts/CharacterLoader.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine.TextCore.Text;
+
+public class CharacterLoader : MonoBehaviour
+{
+    public List<Character> characters;
+
+    void Start()
+    {
+        LoadCharacters();
+        DisplayCharacters();
+    }
+
+    void LoadCharacters()
+    {
+        string path = Path.Combine(Application.streamingAssetsPath, "characters.json");
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            characters = JsonUtility.FromJson<CharacterList>(json).characters;
+        }
+        else
+        {
+            Debug.LogError("Cannot find characters.json file");
+        }
+    }
+
+void DisplayCharacters()
+{
+    foreach (Character character in characters)
+    {
+        Debug.Log($"Name: {character.name}, Health: {character.health}, Attack: {character.attack}, Defense: {character.defense}, Speed: {character.speed}");
+    }
+}
+
+
+
+}
+
+
+[System.Serializable]
+public class CharacterList
+{
+    public List<Character> characters;
+}
+
